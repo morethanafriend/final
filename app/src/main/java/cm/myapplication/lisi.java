a28ffdc672e6823b94f4be978778e4fed32fef66
nihao <846685444@qq.com>
2017-01-15 23:17:58 +0800

    第二次加李四

---
+++ app/src/main/java/cm/myapplication/lisi.java
@@ -0,0 +1,22 @@
+package cm.myapplication;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/1/15.
+ */
+public class lisi extends View{
+    public lisi(Context context) {
+        super(context);
+    }
+
+    public lisi(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public lisi(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
