a5cae04ff5dd1a1a747dc73ffcf43f93220c565e
nihao <846685444@qq.com>
2017-01-15 22:20:01 +0800

    你好

---
+++ app/src/main/java/cm/myapplication/def.java
@@ -0,0 +1,18 @@
+package cm.myapplication;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/1/15.
+ */
+public class def extends ViewGroup{
+    public def(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
